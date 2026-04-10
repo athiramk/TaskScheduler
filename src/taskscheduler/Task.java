1b207d4ed6e3b82b9c42b708abdbca8c22e1beb6
Athira MK <mkathiramk@gmail.com>
2026-04-10 15:27:57 +1000

    added deque, streams, map etc

--- src/taskscheduler/Task.java
+++ src/taskscheduler/Task.java
@@ -1,8 +1,10 @@
 package taskscheduler;
 
 import java.time.Instant;
+import java.util.Objects;
 
-public class Task implements Comparable<Task> {
+//public class Task implements Comparable {
+public class Task {
 	
 	private int id;
 	private String name;
@@ -32,15 +34,28 @@ public class Task implements Comparable<Task> {
 	public Instant getTimestamp() {
 		return timestamp;
 	}
-
+/*
 	@Override
 	public int compareTo(Task other) {
 		return Integer.compare(this.priority, other.priority);
 	}
-	
+*/	
 	@Override
 	public String toString() {
 		return String.format("Task[ID=%d, Priority=%d, Name=%s, Task created = %s]", id, priority, name, timestamp);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || this.getClass() != o.getClass()) return false;
+		Task task = (Task) o;
+		return Objects.equals(this.id, task.id);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.id);
 	}
 
 }
